bb0fd512077f5b3eb4078410bf6e0bfefee29641
Emil Boman <emil@boman.io>
2024-10-06 21:22:22 +0200

    Add type: UserId

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -1,1 +1,6 @@
+use uuid::Uuid;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(transparent))]
+pub struct UserId(Uuid);
