f4dae97f03982dac68f99066df574a0c5e2c6071
Emil Boman <emil@boman.io>
2024-11-15 23:04:53 +0100

    Refactor mqtt server connection

--- crates/mqtt/src/server.rs
+++ crates/mqtt/src/server.rs
@@ -102,10 +102,17 @@ impl Server {
             let (stream, address) = self.tcp_listener.accept().await?;
             let handler = handler.clone();
             let client_id = self.next_client_id.fetch_add(1, Ordering::SeqCst);
+            let (reader, writer) = stream.into_split();
+            let reader = Arc::new(Mutex::new(reader));
+            let writer = Arc::new(Mutex::new(writer));
+            let connection = Connection {
+                client_id,
+                reader: reader.clone(),
+                writer: writer.clone(),
+                handler,
+            };
             tokio::spawn(async move {
-                let (reader, writer) = stream.into_split();
-                let writer = Arc::new(Mutex::new(writer));
-                if let Err(error) = handle_connection(reader, writer, handler, client_id).await {
+                if let Err(error) = connection.handle().await {
                     tracing::error!("Error handling connection from {address}: {error}");
                 }
                 tracing::debug!("Connection from {address} closed");
@@ -124,132 +131,141 @@ impl Server {
     }
 }
 
-async fn handle_connection<T>(
-    mut reader: OwnedReadHalf,
-    writer: Arc<Mutex<OwnedWriteHalf>>,
-    handler: T,
-    client_id: u64,
-) -> Result<()>
+struct Connection<T>
 where
     T: Handler,
 {
-    let mut buffer = BytesMut::with_capacity(4096);
-    loop {
-        let mut read_buffer = [0; 1024];
-        let bytes_read = {
-            reader.readable().await?;
-            let bytes_read = reader.read(&mut read_buffer).await?;
-            if bytes_read == 0 {
-                break Ok(());
-            }
-            bytes_read
-        };
-        buffer.extend_from_slice(&read_buffer[..bytes_read]);
+    client_id: u64,
+    reader: Arc<Mutex<OwnedReadHalf>>,
+    writer: Arc<Mutex<OwnedWriteHalf>>,
+    handler: T,
+}
+
+impl<T> Connection<T>
+where
+    T: Handler,
+{
+    async fn handle(&self) -> Result<()> {
+        let mut buffer = BytesMut::with_capacity(4096);
         loop {
-            match ControlPacket::decode(&buffer[..]) {
-                Ok((packet, remaining_bytes)) => {
-                    match handle_packet(&writer, packet, &handler, client_id).await {
-                        Ok(Action::Respond(packet)) => match packet.encode() {
-                            Ok(packet) => {
-                                let mut writer = writer.lock().await;
-                                writer.write_all(&packet).await?;
+            let mut read_buffer = [0; 1024];
+            let bytes_read = {
+                let mut reader = self.reader.lock().await;
+                reader.readable().await?;
+                let bytes_read = reader.read(&mut read_buffer).await?;
+                if bytes_read == 0 {
+                    break Ok(());
+                }
+                bytes_read
+            };
+            buffer.extend_from_slice(&read_buffer[..bytes_read]);
+            loop {
+                match ControlPacket::decode(&buffer[..]) {
+                    Ok((packet, remaining_bytes)) => {
+                        match self.handle_packet(packet).await {
+                            Ok(Action::Respond(packet)) => match packet.encode() {
+                                Ok(packet) => {
+                                    let mut writer = self.writer.lock().await;
+                                    writer.write_all(&packet).await?;
+                                }
+                                Err(error) => {
+                                    tracing::error!("Error encoding packet: {error}");
+                                    continue;
+                                }
+                            },
+                            Ok(Action::Disconnect) => {
+                                return Ok(());
                             }
+                            Ok(Action::Continue) => {}
                             Err(error) => {
-                                tracing::error!("Error encoding packet: {error}");
+                                tracing::error!("Error handling packet: {error}");
                                 continue;
                             }
-                        },
-                        Ok(Action::Disconnect) => {
-                            return Ok(());
                         }
-                        Ok(Action::Continue) => {}
-                        Err(error) => {
-                            tracing::error!("Error handling packet: {error}");
-                            continue;
+                        if remaining_bytes == 0 {
+                            buffer.clear();
+                            break;
                         }
+                        buffer.advance(buffer.len() - remaining_bytes);
                     }
-                    if remaining_bytes == 0 {
-                        buffer.clear();
+                    Err(crate::protocol::Error::Incomplete) => {
                         break;
                     }
-                    buffer.advance(buffer.len() - remaining_bytes);
-                }
-                Err(crate::protocol::Error::Incomplete) => {
-                    break;
-                }
-                Err(crate::protocol::Error::Invalid) => {
-                    return Ok(());
-                }
-                Err(error) => {
-                    tracing::error!("Error parsing message: {error}");
-                    break;
+                    Err(crate::protocol::Error::Invalid) => {
+                        return Ok(());
+                    }
+                    Err(error) => {
+                        tracing::error!("Error parsing message: {error}");
+                        break;
+                    }
                 }
             }
         }
     }
-}
 
-async fn handle_packet<T>(
-    writer: &Arc<Mutex<OwnedWriteHalf>>,
-    packet: ControlPacket,
-    handler: &T,
-    client_id: u64,
-) -> Result<Action>
-where
-    T: Handler,
-{
-    match packet {
-        ControlPacket::Connect { clean_start } => {
-            let connack = handler
-                .handle_connect(Connect {
-                    client_id,
-                    clean_start,
-                })
-                .await;
-            Ok(Action::Respond(ControlPacket::Connack {
-                reason_code: connack.reason_code,
-            }))
-        }
-        ControlPacket::Publish {
-            topic_name,
-            payload,
-        } => {
-            let _puback = handler
-                .handle_publish(Publish {
-                    topic_name: &topic_name,
-                    payload: &payload,
-                })
-                .await;
-            Ok(Action::Respond(ControlPacket::Puback {}))
-        }
-        ControlPacket::Subscribe {
-            packet_identifier,
-            topic_name,
-        } => {
-            let suback = handler
-                .handle_subscribe(Subscribe {
-                    client_id,
-                    topic_name: &topic_name,
-                })
-                .await;
-            Ok(Action::Respond(ControlPacket::Suback {
+    async fn handle_packet(
+        &self,
+        packet: ControlPacket,
+    ) -> Result<Action>
+    where
+        T: Handler,
+    {
+        match packet {
+            ControlPacket::Connect { clean_start } => {
+                let connack = self
+                    .handler
+                    .handle_connect(Connect {
+                        client_id: self.client_id,
+                        clean_start,
+                    })
+                    .await;
+                Ok(Action::Respond(ControlPacket::Connack {
+                    reason_code: connack.reason_code,
+                }))
+            }
+            ControlPacket::Publish {
+                topic_name,
+                payload,
+            } => {
+                let _puback = self
+                    .handler
+                    .handle_publish(Publish {
+                        topic_name: &topic_name,
+                        payload: &payload,
+                    })
+                    .await;
+                Ok(Action::Respond(ControlPacket::Puback {}))
+            }
+            ControlPacket::Subscribe {
                 packet_identifier,
-                reason_codes: suback.reason_codes.to_vec(),
-            }))
+                topic_name,
+            } => {
+                let suback = self
+                    .handler
+                    .handle_subscribe(Subscribe {
+                        client_id: self.client_id,
+                        topic_name: &topic_name,
+                    })
+                    .await;
+                Ok(Action::Respond(ControlPacket::Suback {
+                    packet_identifier,
+                    reason_codes: suback.reason_codes.to_vec(),
+                }))
+            }
+            ControlPacket::Pingreq => {
+                self.handler.handle_ping().await;
+                Ok(Action::Respond(ControlPacket::Pingresp))
+            }
+            ControlPacket::Disconnect { reason_code } => {
+                self.handler
+                    .handle_disconnect(Disconnect {
+                        client_id: self.client_id,
+                        reason_code,
+                    })
+                    .await;
+                Ok(Action::Disconnect)
+            }
+            _ => todo!(),
         }
-        ControlPacket::Pingreq => {
-            handler.handle_ping().await;
-            Ok(Action::Respond(ControlPacket::Pingresp))
-        }
-        ControlPacket::Disconnect { reason_code } => {
-            handler
-                .handle_disconnect(Disconnect {
-                    client_id,
-                    reason_code,
-                })
-                .await;
-            Ok(Action::Disconnect)
-        }
-        _ => todo!(),
     }
 }
