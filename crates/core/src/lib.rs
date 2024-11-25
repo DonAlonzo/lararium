4478247556fb92fabd174da0b79e7213065b06f0
Emil Boman <emil@boman.io>
2024-11-25 20:20:16 +0100

    Refactor

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,14 +1,15 @@
 pub mod prelude;
 
 mod entry;
-pub use entry::Entry;
 mod filter;
-pub use filter::Filter;
 mod schema;
-pub use schema::Schema;
 mod segment;
-pub use segment::Segment;
 mod topic;
-pub use topic::Topic;
 mod value;
+
+pub use entry::Entry;
+pub use filter::Filter;
+pub use schema::Schema;
+pub use segment::Segment;
+pub use topic::Topic;
 pub use value::Value;
